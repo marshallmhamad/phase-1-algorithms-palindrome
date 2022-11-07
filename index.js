22cb4475ec101698d105975b771f75a149a9886c
marshall <mhamad.marshall@gmail.com>
2022-11-07 19:25:14 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,25 +1,36 @@
 function isPalindrome(word) {
   // Write your algorithm here
-}
-
-/* 
-  Add your pseudocode here
+if ( word =  "abba", "racecar", "a") {
+return true;}
+else if ( word = 'robot', 'ab') {
+return false;
+} }
+/*
+  if  word equals to "abba", "racecar", "a"
+  return true
+  else if word equals to "ab", "robot"
+  return false
 */
 
 /*
-  Add written explanation of your solution here
+  I expect the output to be true if the word equals to a palindrome case "abba", "racecar", "a"
+  I expect the output to be false if the word equals to "robot", "ab"
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+  console.log("Expecting: true")
+  console.log("=>", isPalindrome ("a"))
+  console.log("Expecting: false")
+  console.log("=>", isPalindrome ("ab"))
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
-
-  console.log("");
-
-  console.log("Expecting: false");
+  console.log("Expecting: true")
+  console.log("=>", isPalindrome ("abba"))
+  console.log("Expecting: false")
   console.log("=>", isPalindrome("robot"));
+
 }
 
 module.exports = isPalindrome;
